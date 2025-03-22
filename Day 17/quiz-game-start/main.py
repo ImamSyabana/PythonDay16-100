3a269577d0f69b6c4a2ef74149560490c538ef7c
ImamSyabana <imambritish@gmail.com>
2025-03-22 19:18:22 +0700

    oop quiz brain

--- Day 17/quiz-game-start/main.py
+++ Day 17/quiz-game-start/main.py
@@ -6,13 +6,15 @@ questionList = []
 
 # ngebikin objek yang isinya property soal sama jawaban yang diambil dari dictionary
 for x in range (len(data.question_data)):
-    tempObjek = question_model.Question(data.question_data[x]["text"], data.question_data[x]["answer"])
+    tempObjek = question_model.Question(data.question_data[x]["question"], data.question_data[x]["correct_answer"])
     questionList.append(tempObjek)
     
-
 quizObjek = quiz_brain.QuizBrain(questionList)
-quizObjek.next_question()
 
+while quizObjek.still_has_question() == True:
+    
+    quizObjek.next_question()
+    
+print("You've completed the quiz")
+print("Your final score was: {score}/{outOf}".format(score = quizObjek.score, outOf = quizObjek.question_number))
 
-print(questionList[0].text)
-print(questionList[0].answer)
--- Day 17/quiz-game-start/quiz_brain.py
+++ Day 17/quiz-game-start/quiz_brain.py
@@ -1,9 +1,33 @@
 class QuizBrain:
+    
     def __init__(self, questionBank):
         self.question_number = 0
         self.question_list = questionBank
+        self.score = 0
+        
+    def still_has_question(self):
+        if (self.question_number) < len(self.question_list):
+            return True
+        else:
+            return False
         
     def next_question(self):
-        currentQuestion = self.question_number
-        self.question_number = self.question_number + 1
-        input("Q.{nomersoal}: {soal} (True/False)?".format(nomersoal = currentQuestion, soal = self.question_list[currentQuestion].text))
+        currentNumber = self.question_number
+        user_answer = input("Q.{nomersoal}: {soal} (True/False)? ".format(nomersoal = currentNumber + 1, soal = self.question_list[self.question_number].text))
+        self.check_answer(user_answer, self.question_list[self.question_number].answer)
+        self.question_number= self.question_number + 1
+        
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.title() == correct_answer:
+            print("You got it right")
+            self.score = self.score + 1
+            
+        else:
+            print("That's wrong")
+            
+        print("The correct answer was {ans}".format(ans = correct_answer))
+        
+        print("Your current score is: {score}/{currentQuestion}.".format(score = self.score, currentQuestion = self.question_number + 1))
+        print("\n" * 2)
+        
+            
