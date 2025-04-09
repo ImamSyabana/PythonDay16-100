359ec49d7b2434b86d3d62d049bd91a77b0ca1d9
ImamSyabana <imambritish@gmail.com>
2025-04-09 13:31:02 +0700

    random walk

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,4 +1,6 @@
 from turtle import Turtle, Screen
+import random
+
 ninjaTurtle = Turtle()
 ninjaTurtle.shape("arrow")
 ninjaTurtle.color("darkSlateGray")
@@ -21,22 +23,50 @@ for x in range(20):
 # pause 3 - macam2 bangun datar
 ninjaTurtle.reset()
 
+ninjaTurtle.speed(9)
+
 sidesNumber = 3
 decagon = 10
 
-colorList = ["LightSkyBlue", "LightSkyBlue1", "LightSkyBlue2", "LightSkyBlue3", 
-             "LightSkyBlue4", "LightSlateBlue", "magenta", "magenta2"]
+colorList = ["DarkRed", "Darkorchid4", "lightSalmon", "cadetBlue", 
+             "HotPink", "greenYellow", "magenta4", "navy"]
 
 while sidesNumber < decagon:
     for x in range(sidesNumber):
         ninjaTurtle.forward(100)
         ninjaTurtle.right(360/sidesNumber)
         
-    ninjaTurtle.color(colorList[sidesNumber - 3])
+    ninjaTurtle.color(random.choice(colorList))
     sidesNumber = sidesNumber + 1
     
     
+# pause 4 - random walk
+ninjaTurtle.reset()
+
+ninjaTurtle.pensize(10)
+
+
+def goStraight():
+    ninjaTurtle.forward(20)
     
+def goLeft():
+    ninjaTurtle.left(90)
+    ninjaTurtle.forward(20)
+    
+def goRight():
+    ninjaTurtle.right(90)
+    ninjaTurtle.forward(20)
+    
+def goBackwards():
+    ninjaTurtle.backward(20)
+
+fungsiList = [goRight, goStraight, goBackwards, goLeft]
+
+
+for i in range(200):
+    ninjaTurtle.color(random.choice(colorList))
+    fungsiList[random.randrange(0,4)]()
+
 
 screen = Screen()
 screen.exitonclick()
