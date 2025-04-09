60ac36abc26331affb00d88741d6aa5a8dabaf2a
ImamSyabana <imambritish@gmail.com>
2025-04-09 08:41:36 +0700

    turtle GUI

---
+++ Day 18/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+ninjaTurtle = Turtle()
+ninjaTurtle.shape("arrow")
+ninjaTurtle.color("darkSlateGray")
+
+# Pause number 1
+for x in range(4):
+    ninjaTurtle.forward(100)
+    ninjaTurtle.right(90)
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
