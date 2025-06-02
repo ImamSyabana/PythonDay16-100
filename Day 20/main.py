23a25966f3876867f6adf8232b7ee8da50f3d902
ImamSyabana <imambritish@gmail.com>
2025-06-02 15:57:29 +0700

    snake game

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,6 +1,6 @@
 from turtle import Turtle, Screen
 import time
-import snake
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,12 +8,20 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+snake= Snake() # Creating the snake
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.5)
     
+    snake.move() # Moving the snake
     
     
 screen.exitonclick()
