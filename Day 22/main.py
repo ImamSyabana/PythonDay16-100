3621f7789a60d13f6948b6cf2e44e37d555e60bb
ImamSyabana <imambritish@gmail.com>
2025-06-04 10:11:26 +0700

    snake game

---
+++ Day 22/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width = 800, height= 600)
+screen.bgcolor("black")
+screen.title("pong game")
+
+
+
+screen.exitonclick()
+
+
+
+turtle = Turtle()
+
