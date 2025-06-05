d331697247e95ec39a48a27713fac9f313eb8c46
ImamSyabana <imambritish@gmail.com>
2025-06-06 03:59:42 +0700

    turtle crossing game

--- Day 23/main.py
+++ Day 23/main.py
@@ -12,6 +12,9 @@ screen.tracer(0)
 turtle = Player()
 car_manager = CarManager()
 
+# object score
+scoreboard = Scoreboard()
+
 screen.listen()
 
 screen.onkey(turtle.move_forward, "w")
@@ -26,6 +29,7 @@ while game_is_on:
     if turtle.ycor() > 250:
         turtle.goto(turtle.start_pos)
         tambah_kecepatan = tambah_kecepatan + MOVE_INCREMENT
+        scoreboard.addScore()
     
     car_manager.create_car()
     car_manager.move_sideways(tambah_kecepatan)
@@ -33,6 +37,7 @@ while game_is_on:
     
     for collision in range(len(car_manager.all_cars)):
         if abs(turtle.xcor() - car_manager.all_cars[collision].xcor()) < 25 and abs(turtle.ycor() - car_manager.all_cars[collision].ycor()) < 20:
+            scoreboard.game_terminate_message()
             game_is_on = False
         
     
--- Day 23/scoreboard.py
+++ Day 23/scoreboard.py
@@ -1,5 +1,25 @@
 FONT = ("Courier", 24, "normal")
 
-
-class Scoreboard:
-    pass
+from turtle import Turtle
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(x = -200, y = 250)
+        self.write(arg = "Level: {score}".format(score = self.score), align = "center", font = FONT)
+        
+    def addScore(self):
+        self.score = self.score + 1
+        self.update_scoreboard()
+        
+    def game_terminate_message(self):
+        self.goto(x = 0, y = 0)
+        self.write(arg = "GAME OVER", align="center", font = FONT)
