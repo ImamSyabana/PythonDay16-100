88143708d611890d854fc46e58e293f6fd9c1baf
ImamSyabana <imambritish@gmail.com>
2025-06-09 10:46:25 +0700

    turtle crossing game

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,3 +1,13 @@
+# Reading a file
 with open("Day 24/my_file.txt") as file:
     content = file.read()
     print(content)
+    
+# Writing a file
+with open("Day 24/my_file.txt", mode = "a") as file:
+    file.write("\nnew text")
+    
+# Writing a new file that doest exist 
+with open("Day 24/new_file.txt", mode = "w") as file:
+    file.write("new text")
+    
