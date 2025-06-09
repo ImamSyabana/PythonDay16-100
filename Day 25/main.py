5f7ebb8858815a681c474d7974e8eb87b7bccf75
ImamSyabana <imambritish@gmail.com>
2025-06-09 19:04:22 +0700

    pandas library

--- Day 25/main.py
+++ Day 25/main.py
@@ -39,3 +39,36 @@ temp_list = data["temp"].tolist()
 sum_temp = sum(temp_list[:])
 average_temp = sum_temp / len(temp_list)
 print(average_temp)
+
+# alternatif
+print(data["temp"].mean())
+
+# nilai maksimum
+print(data["temp"].max())
+
+#mendapatkan data di columns
+print(data["condition"])
+print(data.condition) # case sensitive
+
+# get specific data in a row of dataframe 
+print(data[data["day"] == "Monday"])
+
+# mengambil row data yang temperaturnya paling maksimum
+print(data[data["temp"] == data["temp"].max()])
+
+# mendapatkan informasi kolom yang ada pada baris tertentu dalam dataset
+monday = data[data["day"] == "Monday"]
+print(monday.condition) # sunny
+
+# challenge: merubah temperature hari Monday dari celcius ke fahrenheit
+print((int(monday.temp[0]) * 9/5) + 32)
+
+
+## CREATE A DATAFRAME FROM SCRATCH
+data_dict = {
+    "students" : ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pd.DataFrame(data_dict)
+data.to_csv("Day 25/new_data.csv")
