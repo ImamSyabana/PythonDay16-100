f106e981d2925fdd0c98e0a07ac037e8ad6ece02
ImamSyabana <imambritish@gmail.com>
2025-06-13 10:27:59 +0700

    args kwargs

--- Day 27/playground.py
+++ Day 27/playground.py
@@ -1,3 +1,6 @@
+# Unlimited positional arguments
+# unlimited arguments   
+
 def add(*args):
     sum = 0
     for x in args: # args akan berbentuk tupple
@@ -5,4 +8,19 @@ def add(*args):
     return sum
 
 print(add(1,1,1,1,1,2,2,2,3,4,5,6,10))
+
+# Unlimited keywords arguments
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    print(type(kwargs))
     
+    for key, value in kwargs.items():
+        print(key, value)
+        
+    n = n + kwargs["add"]
+    n = n * kwargs["multiply"]
+    
+    return n
+
+print(calculate(2, add = 3, multiply = 5))
