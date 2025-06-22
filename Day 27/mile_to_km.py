0c1d8677a5a8c3a405bdee9fe2a321556f729f2f
ImamSyabana <imambritish@gmail.com>
2025-06-22 10:50:59 +0700

    miles to km

--- Day 27/mile_to_km.py
+++ Day 27/mile_to_km.py
@@ -4,7 +4,8 @@ import tkinter
 window = tkinter.Tk()
 
 window.title("Mile to Km Converter")
-window.minsize(width=500, height= 300)
+window.minsize(width=240, height= 140)
+window.config(padx =20, pady = 20)
 
 # menambahkan padding
 window.config()
@@ -12,14 +13,14 @@ window.config()
 # Create a label (component)
 my_label = tkinter.Label(text = "is equal to")
 my_label.grid(column = 0, row = 1) # untuk pack label into the screen
-my_label.config(padx =20, pady = 0)
+my_label.config(padx =10, pady = 10)
 
 # Button
 
 def miles_to_km():
     #my_label["text"] = "Button Got Clicked"
     km_input.delete(0, tkinter.END)
-    km_input.insert(tkinter.END, string = f"{int(miles_input.get()) * 1.60934}")
+    km_input.insert(tkinter.END, string = f"{float(miles_input.get()) * 1.60934}")
     
 
 
@@ -27,6 +28,7 @@ def miles_to_km():
 miles_input = tkinter.Entry(width=10)
 print(miles_input.get())
 miles_input.grid(column = 1, row = 0)
+
 
 # Entry untuk masukkin km
 km_input = tkinter.Entry(width=10)
@@ -36,7 +38,7 @@ km_input.grid(column = 1, row = 1)
 # Create a label (component)
 miles_label = tkinter.Label(text = "Miles")
 miles_label.grid(column = 2, row = 0) # untuk pack label into the screen
-miles_label.config(padx =0, pady = 10)
+miles_label.config(padx =0, pady = 0)
 
 km_label = tkinter.Label(text = "Km")
 km_label.grid(column = 2, row = 1) # untuk pack label into the screen
@@ -44,4 +46,4 @@ km_label.grid(column = 2, row = 1) # untuk pack label into the screen
 calculate_btn = tkinter.Button(text="Calculate", command = miles_to_km)
 calculate_btn.grid(column=1, row=2)
 
-window.mainloop() # Always at the end of the code
+window.mainloop() # Always at the end of the code.
