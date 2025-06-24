fd4bc03512241806f59f0dcf22c02fd79d6f04cf
ImamSyabana <imambritish@gmail.com>
2025-06-24 20:02:33 +0700

    miles to km

--- Day 28/main.py
+++ Day 28/main.py
@@ -13,9 +13,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    count_down(5 * 60)
+    
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+import math
+def count_down(count):
+    min = math.floor(count / 60)
+    sec = count % 60
+    canvas.itemconfig(timer_text, text = f"{min}:{sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro")
@@ -27,13 +36,16 @@ canvas  = tkinter.Canvas(width = 200, height  = 224, bg = YELLOW, highlightthick
 tomato_img = tkinter.PhotoImage(file = "Day 28/tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
 
-canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row= 2)
+
+
+
 
 title_label = tkinter.Label(text = "Timer", font = (FONT_NAME, 40, "bold"), bg = YELLOW, fg= GREEN)
 title_label.grid(column=1, row= 0)
 
-startButton = tkinter.Button(text = "start")
+startButton = tkinter.Button(text = "start", command= start_timer)
 startButton.grid(column=0, row=3)
 
 resetButton = tkinter.Button(text = "reset")
