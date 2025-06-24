1905cd62f590c21b3e115983ef620a1334c52962
ImamSyabana <imambritish@gmail.com>
2025-06-24 15:20:00 +0700

    miles to km

---
+++ Day 28/main.py
@@ -0,0 +1,44 @@
+import tkinter
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx = 100, pady = 50, bg = YELLOW)
+
+
+canvas  = tkinter.Canvas(width = 200, height  = 224, bg = YELLOW, highlightthickness = 0)
+
+tomato_img = tkinter.PhotoImage(file = "Day 28/tomato.png")
+canvas.create_image(100, 112, image = tomato_img)
+
+canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row= 2)
+
+title_label = tkinter.Label(text = "Timer", font = (FONT_NAME, 40, "bold"), bg = YELLOW, fg= GREEN)
+title_label.grid(column=1, row= 0)
+
+startButton = tkinter.Button(text = "start")
+startButton.grid(column=0, row=3)
+
+resetButton = tkinter.Button(text = "reset")
+resetButton.grid(column=2, row = 3)
+
+checkMark = tkinter.Label(text = "✓", font = (FONT_NAME, 14, "bold"), bg = YELLOW, fg= GREEN)
+checkMark.grid(column = 1, row = 4)
+window.mainloop()
