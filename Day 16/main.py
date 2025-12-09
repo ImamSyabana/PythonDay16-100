da9d6b1f4349aa599d0313a909311bebdf458a41
Imam Sya'bana <imambritish@gmail.com>
2025-12-09 10:19:06 +0700

    Update main.py

--- Day 16/main.py
+++ Day 16/main.py
@@ -12,18 +12,3 @@ print(my_screen.canvheight)
 my_screen.exitonclick()
 
 #reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
-#reboisasi
